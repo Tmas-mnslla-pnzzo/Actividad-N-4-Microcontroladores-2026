087ed94b6da3649b793239f4e05d734a1b3dc419
tomas <tomas@gmail.com>
2026-05-01 23:26:22 -0300

    Primer commit

---
+++ Actividad4/main.c
@@ -0,0 +1,18 @@
+/*
+ * Actividad4.c
+ *
+ * Created: 1/5/2026 23:25:06
+ * Author : tomi
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
