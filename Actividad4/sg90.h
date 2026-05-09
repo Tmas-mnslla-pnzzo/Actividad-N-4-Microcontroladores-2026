2ddb3cfd772ebc0cd03727069fd10475af78ebb5
tomas <tomas@gmail.com>
2026-05-08 21:53:45 -0300

    se agregaron las librerias que faltaban

---
+++ Actividad4/sg90.h
@@ -0,0 +1,16 @@
+/*
+ * sg90.h
+ *
+ * Created: 8/5/2026 21:41:49
+ *  Author: tomi
+ */ 
+
+
+#ifndef SG90_H_
+#define SG90_H_
+
+
+
+
+
+#endif /* SG90_H_ */
---
+++ Actividad4/tcrt5000.h
@@ -0,0 +1,16 @@
+/*
+ * tcrt5000.h
+ *
+ * Created: 8/5/2026 21:42:10
+ *  Author: tomi
+ */ 
+
+
+#ifndef TCRT5000_H_
+#define TCRT5000_H_
+
+
+
+
+
+#endif /* TCRT5000_H_ */
